d289c8f06f300677bab2a9b04142772aa6c55129
Adam Boulal <adam.boulal2@jcmaxwell.it>
2026-04-16 07:23:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-.
+const express = require("express");
+const app = express();
+const usersRoutes = require("./routes/users");
+
+app.use(express.json());
+
+app.use("/api/users", usersRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server attivo su http://localhost:${PORT}`);
+});
